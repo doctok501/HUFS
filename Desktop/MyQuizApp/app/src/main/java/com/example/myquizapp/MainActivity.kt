66056f7e8fb2fdc5465f1213d9403b17c9ff412b
kim_ingyeom <doctok501@gmail.com>
2022-11-04 01:33:56 +0900

    KOTLIN

--- Desktop/MyQuizApp/app/src/main/java/com/example/myquizapp/MainActivity.kt
+++ Desktop/MyQuizApp/app/src/main/java/com/example/myquizapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.myquizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnstart : Button = findViewById(R.id.btn_start)
+        val etname : EditText = findViewById(R.id.et_name)
+        btnstart.setOnClickListener{
+
+            if (etname.text.isEmpty()){
+                Toast.makeText(this,
+                    "pleas enter your name", Toast.LENGTH_LONG).show()
+            }else{
+                val intent = Intent(this, QuizQuestionActivity::class.java)
+                startActivity(intent)
+                finish()
+
+
+            }
+
+        }
+
     }
 }
---
+++ Desktop/MyQuizApp/app/src/main/java/com/example/myquizapp/QuizQuestionActivity.kt
@@ -0,0 +1,55 @@
+package com.example.myquizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+
+class QuizQuestionActivity : AppCompatActivity() {
+
+    private var progressBar : ProgressBar? = null
+    private var tvProgress : TextView? = null
+    private var tvQuestion : TextView? = null
+    private var ivImage : ImageView?= null
+
+    private var tvOptionOne : TextView? = null
+    private var tvOptionTwo : TextView? = null
+    private var tvOptionThree : TextView? = null
+    private var tvOptionFour : TextView? = null
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_question)
+
+        progressBar = findViewById(R.id.progressBar)
+        tvProgress = findViewById(R.id.tv_progress)
+        tvQuestion = findViewById(R.id.tv_question)
+        ivImage = findViewById(R.id.iv_image)
+
+
+        val questionsList = Constans.getQuestions()
+        Log.i("Question size is", "${questionsList.size}")
+
+        for (i in questionsList){
+            Log.e("Question", i.question)
+        }
+
+        var currentPosition = 1
+         val question : Question= questionsList[currentPosition - 1 ]
+        ivImage?.setImageResource(question.image)
+        progressBar?.progress = currentPosition
+        tvProgress?.text = "$currentPosition/${progressBar?.max}"
+        tvQuestion?.text = question.question
+        tvOptionOne?.text = question.optionOne
+        tvOptionTwo?.text = question.optionTwo
+        tvOptionThree?.text = question.optionThree
+        tvOptionFour?.text = question.optionFour
+
+
+    }
+}
