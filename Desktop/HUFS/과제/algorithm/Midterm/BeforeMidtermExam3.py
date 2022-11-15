433b26b15828d6f3e12a5d5194f31905d5c73294
kim_ingyeom <doctok501@gmail.com>
2022-11-16 01:56:32 +0900

    remove file

--- Desktop/HUFS/과제/algorithm/Midterm/BeforeMidtermExam3.py
+++
@@ -1,7 +0,0 @@
-#BS
-def BS (A, a, b, x):
-    if a > b : return -1 
-    m = (a + b)//2
-    if x < A[m]: return BS(A, a, m-1 , x) # a 부터 m-1까지 왼쪽범위
-    elif x > A[m]: return BS(A, m+1, b, x) # m+1 부터 b까지 오른쪽범위
-    else: return m
