815f9286d5702c0052c58bed5392450d51e19910
kim_ingyeom <doctok501@gmail.com>
2022-11-20 22:16:38 +0900

    PHP basic

---
+++ Desktop/php/2.php
@@ -0,0 +1,13 @@
+//while
+<h3>구구단 표</h3>
+<table border ="1" width ="100">
+    <?php
+    $a = 2;
+    $b = 1;
+    while ($b <= 9){
+        $c = $a * $b;
+        echo "<tr><td align = 'center'> $a x $b = $c</td></tr>";
+        $b++;
+    }
+    ?>
+    </table>
